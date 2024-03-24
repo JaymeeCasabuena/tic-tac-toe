bc12c72d6ed46fcfaeef9b102e31a890b6db757f
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-03-24 18:19:59 +1000

    Created Tic Tac Toe Board

---
+++ src/components/Board.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+const tiles = ["0", "0", "X", "X", "0", "", "X", "", "0"];
+
+export default function Board () {
+  return (
+      <View style={[styles.outerDiv, styles.outerBorders]}>
+        {tiles.map((t, i) => (
+          <View style={styles.squares} key={i}>
+            <Text style={styles.text}>{t}</Text>
+          </View>
+        ))}
+      </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    outerDiv: {
+      backgroundColor: "#DACC00",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-between",
+      width: 300,
+      height: 300,
+      padding: 25,
+    },
+    squares: {
+      width: "33.33%",
+      height: "33.33%",
+      backgroundColor: "#7AB844",
+      borderColor: "#000000",
+      borderWidth: 2,
+    },
+    outerBorders: {
+      borderWidth: 1,
+      borderColor: "#000000",
+      borderRadius: 10,
+    },
+    text: {
+      position: "absolute",
+      width: "100%",
+      textAlign: "center",
+      fontSize: 60,
+      color: "black",
+      fontFamily: 'Pixel'
+    },
+  });
