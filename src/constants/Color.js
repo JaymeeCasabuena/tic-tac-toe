4b024cbea95645e1de011c56b8c74fb2871afe03
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-04-06 22:58:39 +1000

    Modified color.js

--- src/constants/Color.js
+++ src/constants/Color.js
@@ -1,8 +1,8 @@
 export const COLORS = {
     Red: '#be5d27',
-    Teal: '#6fcb9f',
-    Yellow: '#dacc00',
-    Green: '#7ab844',
+    Teal: '#529f82',
+    Yellow: '#d9a739',
+    Green: '#7e9953',
     Brown: '#666547',
-    Cream: '#fffeb3',
+    Cream: '#f2dbaf',
   };
